40d80c9c3efbcecc1329eb1be30334303a8414ff
Shahrukh Sayyed <ssayyed@griffyn.io>
2020-04-07 15:33:34 +0530

    Change Log 1. CSS updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyD9jXPcQD7VW-pyubGQMo3XaDUjk4XfQS8",
-    authDomain: "dreams-constructions.firebaseapp.com",
-    databaseURL: "https://dreams-constructions.firebaseio.com",
-    projectId: "dreams-constructions",
-    storageBucket: "dreams-constructions.appspot.com",
-    messagingSenderId: "1069859951863",
-    appId: "1:1069859951863:web:f76db1d95a14bca9226ff2"
-  }
+ 
 };
